f831879c761da28c2f5c8a11ac862950c1699ab0
eunsoo8606 <eunsoo8606@gmail.com>
2026-03-02 16:15:13 +0900

    refactor: `dotenv` 설정 호출 위치를 파일 상단으로 이동합니다.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 const nodemailer = require('nodemailer');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
